617f88a583cd03a3b737ba1b2b9e1917a0080e22
Luke Smith <smit5285@purdue.edu>
2026-03-05 19:22:56 -0500

    Created Login screen and created (hopefully) working endpoints

--- app/frontend/src/app/loginScreen.tsx
+++ app/frontend/src/app/loginScreen.tsx
@@ -3,23 +3,46 @@ import LoginButton from '../components/ForgeButton';
 import LoginTextBox from '../components/ForgeTextBox';
 import { StyleSheet, View, Text } from 'react-native';
 import { useRouter } from 'expo-router/build/exports';
+import Constants from 'expo-constants';
+
+const BASE_URL = __DEV__ 
+    ? `http://${Constants.expoConfig?.hostUri?.split(':')[0]}:8000`
+    : 'https://example.com';
 
 const LoginScreen = () => {
     const router = useRouter();
-    const [username, setUsername] = React.useState('');
+    const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
-    const handleLogin = () => {
-        // Implement login logic here
+    const handleLogin = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/auth/login`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                alert(`Error: ${data.detail}`);
+            } else {
+                console.log(data.access_token);
+                console.log(data.refresh_token);
+                router.push('/homeScreen');
+            }
+        } catch (error) {
+            alert('Server did not connect properly.');
+        }
     }
     return (
         <View style = {styles.container}>
             <Text style={styles.title}>Login</Text>
             <LoginTextBox
-                label="Username"
-                value={username}
-                onChangeText={setUsername}
-                maxLength={20}
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
             />
             <LoginTextBox
                 label="Password"
