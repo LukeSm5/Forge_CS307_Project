b65d262de33d8de6bb32fb0d4a6453e418ad6247
Luke Smith <smit5285@purdue.edu>
2026-03-06 09:44:43 -0500

    Unrouted create account

--- app/frontend/src/app/createAccountScreen.tsx
+++ app/frontend/src/app/createAccountScreen.tsx
@@ -53,7 +53,7 @@ const CreateAccountScreen = () => {
                     username: username,
                 });
                 setLoggedIn(true);
-                router.push('/(tabs)/onboarding');
+                router.push('/(tabs)');
             }
         } catch (error) {
             console.log('Full error:', error);
