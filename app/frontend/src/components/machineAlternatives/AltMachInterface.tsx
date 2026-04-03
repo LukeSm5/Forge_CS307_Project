70b53be087d517006009674080fd5322b61c2e3c
Lemon-Chad <jaolsm6296@gmail.com>
2026-04-02 21:35:25 -0400

    Popup UI to display LLM results

---
+++ app/frontend/src/components/machineAlternatives/AltMachInterface.tsx
@@ -0,0 +1,113 @@
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import { Text, View } from '@/components/Themed';
+import AltMachResult from '@/components/cardioSearch/CardioMachineResult';
+import React, { useEffect, useState } from 'react';
+import { api, AltMachResponse } from '@/core/api';
+import ForgeButton from '../ForgeButton';
+import { Modal } from 'react-native';
+
+export default function AltMachInterface({ visible, setVisible, exercise }: { visible: boolean, setVisible: (visible: boolean) => void, exercise: string }) {
+    if (!visible)
+        return (<></>);
+
+    const [ results, setResults ] = useState<AltMachResponse[]>([]);
+
+    let searchComponent: React.JSX.Element;
+    if (results.length > 0) {
+        searchComponent = (<><View style={styles.searchResults}>
+            {results.map((item: AltMachResponse, idx: number) => <AltMachResult key={idx} name={item.name} desc={item.desc} />)}
+        </View></>);
+    } else {
+        searchComponent = (<><View style={styles.searchResults}>
+                <Text style={styles.title}>Loading results...</Text>
+        </View></>);
+    }
+
+    useEffect(() => {
+        api.machineAlternative({ exercise }).then(setResults).catch(alert);
+    }, [exercise]);
+
+    return (<Modal>
+        <View style={styles.container} lightColor="#0007" darkColor="#fff7">
+            <View style={styles.popup}>
+                <Text style={styles.title}>Exercise Alternatives</Text>
+                <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+                {searchComponent}
+                <ForgeButton text="Close Exercise Alternatives" onPress={() => setVisible(false)}/>
+            </View>
+        </View>
+    </Modal>);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFill,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100,
+    },
+    popup: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '75%',
+        marginVertical: '3%',
+        borderRadius: '15px',
+        overflowX: 'hidden',
+        overflowY: 'scroll',
+        padding: '2%',
+        zIndex: 100,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+    button: {
+        backgroundColor: '#007AFF',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        marginVertical: 10,
+    },
+    searchButton: {
+        backgroundColor: '#007AFF',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        width: '30%',
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 12,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    searchResults: {
+        width: '80%',
+        height: '45%',
+        overflowX: 'hidden',
+        overflowY: 'scroll',
+        boxShadow: 'inset 3px 3px 10px #0007',
+        borderRadius: '10px',
+        marginBottom: 10,
+        padding: '2%'
+    },
+    questionContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    questionText: {
+        fontSize: 17,
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+});
---
+++ app/frontend/src/components/machineAlternatives/AltMachResult.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '@/components/Themed';
+
+export default function AltMachResult({ name, desc }: { name: string, desc: string }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{name}</Text>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <Text>{desc}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        boxShadow: '3px 3px 10px #0007',
+        margin: '3%',
+        padding: '3%',
+        width: '95%',
+        borderRadius: '10px'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 10,
+        height: 1,
+        width: '80%',
+    },
+    button: {
+        backgroundColor: '#007AFF',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
