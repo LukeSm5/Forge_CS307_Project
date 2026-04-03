23dc743d8e98d21862469c03cf78e95a7caafb20
Artem <67975795+Artem19284@users.noreply.github.com>
2026-04-03 01:36:48 -0400

    Update test_index tab to include help features for exercises

--- app/frontend/src/app/(tabs)/test_index.tsx
+++ app/frontend/src/app/(tabs)/test_index.tsx
@@ -1,22 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import CardioButton from '@/components/cardioSearch/CardioButton';
-import ProgressionButton from '@/components/workoutProgression/ProgressButton';
-import GymMapButton from '@/components/gymMap/GymMapButton';
-import AltMachButton from '@/components/machineAlternatives/AltMachButton';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import CardioButton from "@/components/cardioSearch/CardioButton";
+import ProgressionButton from "@/components/workoutProgression/ProgressButton";
+import GymMapButton from "@/components/gymMap/GymMapButton";
+import AltMachButton from "@/components/machineAlternatives/AltMachButton";
+import ExerciseButton from "@/components/exerciseHelp/ExerciseButton";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <CardioButton/>
-      <GymMapButton/>
-      <ProgressionButton exerciseId="bicep curl"/>
-      <AltMachButton exercise="barbell bench press"/>
+      <CardioButton />
+      <GymMapButton />
+      <ProgressionButton exerciseId="bicep curl" />
+      <AltMachButton exercise="barbell bench press" />
+      <ExerciseButton />
     </View>
   );
 }
@@ -24,16 +30,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
