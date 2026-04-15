8a838a59e63d34f6152e009db38b2a903ccfb5d7
Lemon-Chad <jaolsm6296@gmail.com>
2026-04-15 14:13:46 -0400

    Date, interactions on notifications

--- app/frontend/src/components/notifications/NotificationComponent.tsx
+++ app/frontend/src/components/notifications/NotificationComponent.tsx
@@ -1,15 +1,63 @@
 import { StyleSheet } from 'react-native';
 
-import { Text, useScheme, View } from '@/components/Themed';
+import { Separator, Text, useScheme, View } from '@/components/Themed';
 import { Notification } from '@/core/api';
+import ForgeButton from '../ForgeButton';
 
 export default function NotificationComponent({ notification }: { notification: Notification }) {
     const s = useScheme();
+
+    const interactions: NotificationInteraction[] = [];
+    switch (notification.type) {
+        case 'friend_request':
+            interactions.push({
+                label: 'Accept',
+                onPress: () => alert('Accepting friend request not implemented yet.')
+            });
+            interactions.push({
+                label: 'Decline',
+                onPress: () => alert('Declining friend request not implemented yet.')
+            });
+            break;
+        case 'view_post':
+            interactions.push({
+                label: 'View Post',
+                onPress: () => alert('Viewing post not implemented yet.')
+            });
+            break;
+        default:
+            break;
+    }
+
+    let interactionsComponent = <></>;
+    if (interactions.length > 0) {
+        interactionsComponent = (
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', gap: 10 }}>
+                {interactions.map((interaction, idx) => (
+                    <ForgeButton key={idx} text={interaction.label} onPress={interaction.onPress} />
+                ))}
+            </View>
+        );
+    }
+
     return (
         <View style={{ ...styles.container, boxShadow: `3px 3px 10px ${s.shadow}`, }}>
-            <Text style={styles.title}>{notification.message}</Text>
+            <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start' }}>
+                <Text style={{ marginBottom: 10 }}>{new Date(notification.timestamp).toLocaleString()}</Text>
+                <Text style={styles.title}>{notification.message}</Text>
+            </View>
+            <Separator />
+            <View style={{ flexDirection: 'row', justifyContent: interactions.length > 0 ? 'space-between' : 'flex-end', alignItems: 'center' }}>
+                {interactionsComponent}
+                <ForgeButton style={{  }} text="Dismiss" onPress={() => alert('Dismissing notification not implemented yet.')} />
+            </View>
         </View>
     );
+}
+
+type NotificationInteraction = {
+    label: string;
+    onPress: () => void;
 }
 
 const styles = StyleSheet.create({
@@ -22,10 +70,5 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 10,
-        height: 1,
-        width: '80%',
     },
 });
