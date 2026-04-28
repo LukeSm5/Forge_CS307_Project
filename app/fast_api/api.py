344a301e0cc23f966ed267c27f87de7e54786adc
P3NX1L <65290636+P3NX1L@users.noreply.github.com>
2026-04-28 10:27:29 -0500

    Revert machine_id a required field in SessionExerciseOut
    Changed machine_id from Optional[int] to int to enforce required field.

--- app/fast_api/api.py
+++ app/fast_api/api.py
@@ -325,7 +325,7 @@ class CreateSessionRequest(BaseModel):
 class SessionExerciseOut(BaseModel):
     exercise_id: int
     exercise_name: str
-    machine_id: Optional[int] = None
+    machine_id: int
     set_number: int
     reps: int
     weight: Optional[int]
